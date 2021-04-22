4100700be0c6ea1d7fabe033e327b1fc8a888a75
o-shibata-dev <lecture.act.os010@gmail.com>
2021-04-22 15:09:59 +0000

    ログイン機能を実装する

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,11 +13,11 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # Success (valid params)
+      # GET "/users/#{user.id}"
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
-      # Failure (not valid params)
       render 'new'
     end
   end
