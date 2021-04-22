d994ad6c1df215f6d9196cabd6d6e4c9183d12a0
o-shibata-dev <lecture.act.os010@gmail.com>
2021-04-22 12:09:05 +0000

    サインアップを実装する

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,30 @@
 class UsersController < ApplicationController
+  # GET /users/:id
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  # GET /users/new
   def new
+    @user = User.new
+  end
+  
+  # POST /users (+ paramas)
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # Success (valid params)
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      # Failure (not valid params)
+      render 'new'
+    end
+  end
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
