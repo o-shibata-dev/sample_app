864c42b6a836d64d14cb12bf8a60c69cd06f0550
o-shibata-dev <lecture.act.os010@gmail.com>
2021-05-27 08:51:29 +0000

    アカウントを有効化する

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,6 +33,9 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+  
+  host = '9e33a70bc1154bf089f9892c73058268.vfs.cloud9.ap-northeast-1.amazonaws.com'
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
 
   config.action_mailer.perform_caching = false
 
